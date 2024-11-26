d23b063b0bca3258f4aa590bf035404ab537ed92
ryan-the-crayon <157742211+ryan-the-crayon@users.noreply.github.com>
2024-11-26 18:43:07 -0500

    Initial work for lms pull (#108)
    * Downloading artifacts
    
    * Fix documentation

---
+++ src/downloadPbUpdater.ts
@@ -0,0 +1,48 @@
+import { text } from "@lmstudio/lms-common";
+import { type DownloadProgressUpdate } from "@lmstudio/sdk";
+import { formatSizeBytes1000 } from "./formatSizeBytes1000.js";
+import { type ProgressBar } from "./ProgressBar.js";
+
+function formatRemainingTime(timeSeconds: number) {
+  const seconds = timeSeconds % 60;
+  const minutes = Math.floor(timeSeconds / 60) % 60;
+  const hours = Math.floor(timeSeconds / 3600);
+  if (hours > 0) {
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  }
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+
+/**
+ * Given a progress bar pb, return a function that updates the progress bar with the given
+ * DownloadProgressUpdate.
+ */
+export function createDownloadPbUpdater(pb: ProgressBar) {
+  let longestDownloadedBytesStringLength = 6;
+  let longestTotalBytesStringLength = 6;
+  let longestSpeedBytesPerSecondStringLength = 6;
+  return ({ downloadedBytes, totalBytes, speedBytesPerSecond }: DownloadProgressUpdate) => {
+    const downloadedBytesString = formatSizeBytes1000(downloadedBytes);
+    if (downloadedBytesString.length > longestDownloadedBytesStringLength) {
+      longestDownloadedBytesStringLength = downloadedBytesString.length;
+    }
+    const totalBytesString = formatSizeBytes1000(totalBytes);
+    if (totalBytesString.length > longestTotalBytesStringLength) {
+      longestTotalBytesStringLength = totalBytesString.length;
+    }
+    const speedBytesPerSecondString = formatSizeBytes1000(speedBytesPerSecond);
+    if (speedBytesPerSecondString.length > longestSpeedBytesPerSecondStringLength) {
+      longestSpeedBytesPerSecondStringLength = speedBytesPerSecondString.length;
+    }
+    const timeLeftSeconds = Math.round((totalBytes - downloadedBytes) / speedBytesPerSecond);
+    pb.setRatio(
+      downloadedBytes / totalBytes,
+      text`
+        ${downloadedBytesString.padStart(longestDownloadedBytesStringLength)} /
+        ${totalBytesString.padStart(longestTotalBytesStringLength)} |
+        ${speedBytesPerSecondString.padStart(longestSpeedBytesPerSecondStringLength)}/s | ETA
+        ${formatRemainingTime(timeLeftSeconds)}
+      `,
+    );
+  };
+}
---
+++ src/subcommands/pull.ts
@@ -0,0 +1,90 @@
+import { text } from "@lmstudio/lms-common";
+import { kebabCaseRegex } from "@lmstudio/lms-shared-types";
+import { command, positional, string, type Type } from "cmd-ts";
+import { resolve } from "path";
+import { createClient, createClientArgs } from "../createClient.js";
+import { createDownloadPbUpdater } from "../downloadPbUpdater.js";
+import { exists } from "../exists.js";
+import { createLogger, logLevelArgs } from "../logLevel.js";
+import { optionalPositional } from "../optionalPositional.js";
+import { ProgressBar } from "../ProgressBar.js";
+
+const artifactIdentifierType: Type<string, { owner: string; name: string }> = {
+  async from(str) {
+    str = str.trim().toLowerCase();
+    const parts = str.split("/");
+    if (parts.length !== 2) {
+      throw new Error("Invalid artifact identifier. Must be in the form of 'owner/name'.");
+    }
+    const [owner, name] = parts;
+    if (!kebabCaseRegex.test(owner)) {
+      throw new Error("Invalid owner. Must be kebab-case.");
+    }
+    if (!kebabCaseRegex.test(name)) {
+      throw new Error("Invalid name. Must be kebab-case.");
+    }
+    return { owner, name };
+  },
+};
+
+export const pull = command({
+  name: "pull",
+  description: "Pull an artifact from LM Studio Hub to a local folder.",
+  args: {
+    artifactIdentifier: positional({
+      displayName: "artifact-identifier",
+      description: "The identifier for the artifact. Must be in the form of 'owner/name'.",
+      type: artifactIdentifierType,
+    }),
+    path: optionalPositional({
+      displayName: "path",
+      description: text`
+        The path to the folder to pull the resources into. If not provided, defaults to a new folder
+        with the artifact name in the current working directory.
+      `,
+      type: string,
+      default: "",
+    }),
+    ...logLevelArgs,
+    ...createClientArgs,
+  },
+  handler: async args => {
+    const logger = createLogger(args);
+    const client = await createClient(logger, args);
+    const { owner, name } = args.artifactIdentifier;
+    let path = args.path;
+    let autoNamed: boolean;
+    if (path === "") {
+      path = resolve(`./${name}`);
+      autoNamed = true;
+      logger.debug(`Path not provided. Using default: ${path}`);
+    } else {
+      path = resolve(path);
+      autoNamed = false;
+      logger.debug(`Using provided path: ${path}`);
+    }
+    if (await exists(path)) {
+      logger.error(`Path already exists: ${path}`);
+      if (autoNamed) {
+        logger.error("You can provide a different path by providing it as a second argument.");
+      }
+      process.exit(1);
+    }
+    const pb = new ProgressBar(0, "", 22);
+    const updatePb = createDownloadPbUpdater(pb);
+    await client.repository.downloadArtifact({
+      owner,
+      name,
+      revisionNumber: -1, // -1 means the latest revision.
+      path,
+      onProgress: update => {
+        updatePb(update);
+      },
+      onStartFinalizing: () => {
+        pb.stop();
+        logger.info("Finalizing download...");
+      },
+    });
+    logger.info(`Artifact successfully pulled to ${path}.`);
+  },
+});
