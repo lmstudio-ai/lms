5547dea1554500cc05d8bd4bd9693f1f00a66a57
ryan-the-crayon <157742211+ryan-the-crayon@users.noreply.github.com>
2025-04-16 15:21:37 -0400

    Automatically generate manifest.json when pushing a virtual model (#211)

--- src/findProjectFolder.ts
+++ src/findProjectFolder.ts
@@ -3,19 +3,24 @@ import { access } from "fs/promises";
 import { dirname, join, resolve } from "path";
 
 /**
- * From the given folder, recursively travels back up, until finds one folder with manifest.json.
+ * From the given folder, recursively travels back up, until finds one folder that contains a file
+ * with the given name.
  */
-export async function findProjectFolder(logger: SimpleLogger, cwd: string) {
+export async function recursiveFindAncestorFolderWithFile(
+  logger: SimpleLogger,
+  fileName: string,
+  cwd: string,
+) {
   let currentDir = resolve(cwd);
 
   let maximumDepth = 20;
   while (maximumDepth > 0) {
     maximumDepth--;
-    const manifestPath = join(currentDir, "manifest.json");
+    const manifestPath = join(currentDir, fileName);
     logger.debug("Trying to access", manifestPath);
     try {
       await access(manifestPath);
-      logger.debug("Found manifest.json at", currentDir);
+      logger.debug(`Found ${fileName} at`, currentDir);
       return currentDir;
     } catch (err) {
       const parentDir = dirname(currentDir);
@@ -26,12 +31,16 @@ export async function findProjectFolder(logger: SimpleLogger, cwd: string) {
       currentDir = parentDir;
     }
   }
-  logger.debug("Reached maximum depth without finding manifest.json");
+  logger.debug(`Reached maximum depth without finding ${fileName}`);
   return null;
 }
 
+/**
+ * Try to find the ancestor folder with a manifest.json file. If it does not exist, print an error
+ * message and exit the process.
+ */
 export async function findProjectFolderOrExit(logger: SimpleLogger, cwd: string) {
-  const projectFolder = await findProjectFolder(logger, cwd);
+  const projectFolder = await recursiveFindAncestorFolderWithFile(logger, "manifest.json", cwd);
   if (projectFolder === null) {
     logger.errorText`
       Could not find the project folder. Please invoke this command in a folder with a
