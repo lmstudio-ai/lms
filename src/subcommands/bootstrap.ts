910d38a950b13944411882e902702069be3a4f8a
rugvedS07 <rugved@lmstudio.ai>
2025-07-16 11:56:11 -0400

    Add an arg to skip confirmation for bootstrap (#268)
    * Add an arg to skip confirmation
    
    * Consistent flag name

--- src/subcommands/bootstrap.ts
+++ src/subcommands/bootstrap.ts
@@ -1,12 +1,19 @@
 import { installCli } from "@lmstudio/lms-lmstudio/install-cli";
-import { command } from "cmd-ts";
+import { command, flag } from "cmd-ts";
 import { platform } from "os";
 
 export const bootstrap = command({
   name: "bootstrap",
   description: "Bootstrap the CLI",
-  args: {},
-  handler: async () => {
-    await installCli({ skipConfirmation: platform() !== "linux" });
+  args: {
+    skipConfirmation: flag({
+      long: "yes",
+      short: "y",
+      description: "Skip confirmation prompts",
+      defaultValue: () => false,
+    }),
+  },
+  handler: async args => {
+    await installCli({ skipConfirmation: args.skipConfirmation || platform() !== "linux" });
   },
 });
