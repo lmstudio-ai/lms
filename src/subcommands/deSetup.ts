389cc3b5731030be22442a62bf01fad79a8ecb7b
Ryan Huang <ryan@lmstudio.ai>
2026-04-03 16:21:38 -0400

    setup and de setup

---
+++ src/subcommands/deSetup.ts
@@ -0,0 +1,20 @@
+import { Command, type OptionValues } from "@commander-js/extra-typings";
+import { text } from "@lmstudio/lms-common";
+import { createClient } from "../createClient.js";
+import { addLogLevelOptions, createLogger, type LogLevelArgs } from "../logLevel.js";
+
+type DeSetupCommandOptions = OptionValues & LogLevelArgs;
+
+const deSetupCommand = new Command<[], DeSetupCommandOptions>()
+  .name("de-setup")
+  .description(text`Remove compute-device setup from this machine`);
+
+addLogLevelOptions(deSetupCommand);
+
+deSetupCommand.action(async (options: DeSetupCommandOptions) => {
+  const logger = createLogger(options);
+  await using client = await createClient(logger);
+  await client.repository.lmLink.unstable_deSetupComputeDevice();
+});
+
+export const deSetup = deSetupCommand;
---
+++ src/subcommands/setup.ts
@@ -0,0 +1,26 @@
+import { Command, type OptionValues } from "@commander-js/extra-typings";
+import { text } from "@lmstudio/lms-common";
+import { createClient } from "../createClient.js";
+import { addLogLevelOptions, createLogger, type LogLevelArgs } from "../logLevel.js";
+
+type SetupCommandOptions = OptionValues & LogLevelArgs;
+
+const setupCommand = new Command<[], SetupCommandOptions>()
+  .name("setup")
+  .description(text`Set up this machine as a compute device`)
+  .argument(
+    "<setup-code>",
+    text`
+      The compute-device setup code from LM Studio.
+    `,
+  );
+
+addLogLevelOptions(setupCommand);
+
+setupCommand.action(async (setupCode: string, options: SetupCommandOptions) => {
+  const logger = createLogger(options);
+  await using client = await createClient(logger);
+  await client.repository.lmLink.unstable_setupComputeDevice(setupCode);
+});
+
+export const setup = setupCommand;
